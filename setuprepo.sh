cd8e187ad07cef8b8a175a9993805c92f28469ce
Rabby Sheikh <nullsqu4d.ng1nx4O4@gmail.com>
2019-09-03 21:34:53 +0600

    Update setuprepo.sh

--- setuprepo.sh
+++ setuprepo.sh
@@ -2,6 +2,7 @@
 apt update &> /dev/null;
 apt install gpg &> /dev/null;
 curl -s http://package.andromux.ml/key.gpg | apt-key add -
+touch $PREFIX/etc/apt/source.list.d/andromuxtest.list
 echo "deb http://package.andromux.ml/ information-gathering termux" > $PREFIX/etc/apt/source.list.d/andromuxtest.list
 apt update
 
